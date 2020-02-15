0200112e350f826b7817f055f1a52cdb2a70175b
tashenoff <kail.dead.kd@gmail.com>
2020-02-15 19:10:47 +0600

    правки после инициализации

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 'use strict';
-
+const appVersion = require('./package.json').version;
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const envify = require('gulp-envify');
 const sass = require('gulp-sass');
 const del = require('del');
 const autoprefixer = require('autoprefixer');
@@ -18,163 +19,202 @@ const sourcemaps = require('gulp-sourcemaps');
 const webp = require('gulp-webp');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const nunjucks = require('gulp-nunjucks');
+//const nunjucks = require('gulp-cache');
+
+const getEnvObject = () => {
+  return envify({
+    appVersion,
+  });
+};
 
 // Очистка папки dist
 function clean() {
-    return del(['./dist']);
+  return del(['./dist']);
 }
 
 // Инициализация веб-сервера
 function browserSyncInit(done) {
-    browserSync.init({
-        server: {
-            baseDir: './dist',
-        },
-        // tunnel: true,
-        host: 'localhost',
-        port: 9000,
-        logPrefix: 'Webit_Dev',
-    });
-    done();
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+    },
+    // tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: 'Webit_Dev',
+  });
+  done();
 }
 
 // Функция перезагрузки страницы
 function browserSyncReload(done) {
-    browserSync.reload();
-    done();
+  browserSync.reload();
+  done();
 }
 
 // Шаблонизация и склейка HTML
 function htmlProcess() {
-    return gulp
-        .src(['src/*.html'])
-        .pipe(nunjucks.compile())
-        .pipe(gulp.dest('./dist/'));
+  return gulp
+    .src(['src/*.html'])
+    .pipe(nunjucks.compile())
+    .pipe(getEnvObject())
+    .pipe(gulp.dest('./dist/'));
 }
 
 function imgProcessWebp() {
-    return gulp
-        .src('src/img/**/*.*')
-        .pipe(webp())
-        .pipe(gulp.dest('dist/img/'));
+  return gulp
+    .src('src/img/**/*.*')
+    .pipe(webp())
+    .pipe(gulp.dest(`dist/assets/${appVersion}/img/`));
 }
 
 function imgProcessWatch() {
-    return gulp
-        .src('src/img/**/*.*')
-        .pipe(changed('dist/img/'))
-        .pipe(gulp.dest('dist/img/'));
+  return gulp
+    .src('src/img/**/*.*')
+    .pipe(changed('dist/img/'))
+    .pipe(gulp.dest(`dist/assets/${appVersion}/img/`));
 }
 
 // Работа с картинками для Production
 function imgProcessBuild() {
-    return gulp
-        .src('src/img/**/*.*')
-        .pipe(imagemin([
-            imageminMozjpeg({
-                quality: 90
-            })
-        ]))
-        .pipe(gulp.dest('dist/img/'));
+  return gulp
+    .src('src/img/**/*.*')
+    .pipe(
+      imagemin([
+        imageminMozjpeg({
+          quality: 90,
+        }),
+      ]),
+    )
+    .pipe(gulp.dest(`dist/assets/${appVersion}/img/`));
 }
 
 // Копирование шрифтов
 function fontsProcess() {
-    return gulp.src(['src/fonts/**/*']).pipe(gulp.dest('./dist/fonts'));
+  return gulp
+    .src(['src/fonts/**/*'])
+    .pipe(gulp.dest(`./dist/assets/${appVersion}/fonts`));
 }
 
 // Склейка и минификация CSS
 function cssProcess() {
-    var plugins = [autoprefixer(), cssnano()];
-    return gulp
-        .src(['src/css/reset.css', 'src/css/grid.css', 'src/css/**/*.*'])
-        .pipe(concat('libs.min.css'))
-        .pipe(postcss(plugins))
-        .pipe(gulp.dest('./dist/css'));
+  var plugins = [autoprefixer(), cssnano()];
+  return gulp
+    .src(['src/css/reset.css', 'src/css/**/*.*'])
+    .pipe(concat('libs.min.css'))
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest(`./dist/assets/${appVersion}/css`));
 }
 
 // Компиляция SASS
 function scssProcess() {
-    var plugins = [autoprefixer()];
-    return gulp
-        .src(['src/sass/app.scss'])
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(postcss(plugins))
-        .pipe(prettier({singleQuote: true}))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./dist/css'));
+  var plugins = [autoprefixer()];
+  return gulp
+    .src(['src/sass/app.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss(plugins))
+    .pipe(prettier({ singleQuote: true }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest(`./dist/assets/${appVersion}/css`));
 }
 
 // Компиляция SASS  для Production
 function scssProcessBuild() {
-    var plugins = [autoprefixer(),cssnano()];
-    return gulp
-        .src(['src/sass/app.scss'])
-        .pipe(sass())
-        .pipe(postcss(plugins))
-        .pipe(gulp.dest('./dist/css'));
+  var plugins = [autoprefixer(), cssnano()];
+  return gulp
+    .src(['src/sass/app.scss'])
+    .pipe(sass())
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest(`dist/assets/${appVersion}/css`));
 }
 
 // Склейка и минификация JS библиотек
 function libsJsProcess() {
-    return gulp
-        .src([
-            'node_modules/jquery/dist/jquery.min.js',
-            'src/js/!(app)*.js',
-        ])
-        .pipe(concat('libs.min.js'))
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'));
+  return gulp
+    .src(['node_modules/jquery/dist/jquery.min.js', 'src/js/!(app)*.js'])
+    .pipe(concat('libs.min.js'))
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(gulp.dest(`./dist/assets/${appVersion}/js`));
 }
 
 // Склейка пользовательского JS
 function jsProcess() {
-    return (
-        gulp
-            .src(['src/js/app.js'])
-            .pipe(beautify())
-            .pipe(gulp.dest('./dist/js'))
-    );
+  return gulp
+    .src(['src/js/app.js'])
+    .pipe(beautify())
+    .pipe(gulp.dest(`./dist/assets/${appVersion}/js`));
 }
 
 // Компиляция JS  для Production
 function jsProcessBuild() {
-    return (
-        gulp
-            .src(['src/js/app.js'])
-            .pipe(babel())
-            .pipe(uglify())
-            .pipe(gulp.dest('./dist/js'))
-    );
+  return gulp
+    .src(['src/js/app.js'])
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(gulp.dest(`dist/assets/${appVersion}/js`));
 }
 
 // Наблюдение за изменениями
 function watchFiles() {
-    gulp.watch('src/**/*.html', gulp.series(htmlProcess, browserSyncReload));
-    gulp.watch('src/css/**/*.*', gulp.series(cssProcess, browserSyncReload));
-    gulp.watch('src/sass/**/*.*', gulp.series(scssProcess, browserSyncReload));
-    gulp.watch('src/js/!(app)*.js', gulp.series(libsJsProcess, browserSyncReload));
-    gulp.watch('src/js/app.js', gulp.series(jsProcess, browserSyncReload));
-    gulp.watch('src/img/**/*.*', gulp.series(imgProcessWatch, browserSyncReload));
-    gulp.watch('src/fonts/*.*', gulp.series(fontsProcess, browserSyncReload));
+  gulp.watch('src/**/*.html', gulp.series(htmlProcess, browserSyncReload));
+  gulp.watch('src/css/**/*.*', gulp.series(cssProcess, browserSyncReload));
+  gulp.watch('src/sass/**/*.*', gulp.series(scssProcess, browserSyncReload));
+  gulp.watch(
+    'src/js/!(app)*.js',
+    gulp.series(libsJsProcess, browserSyncReload),
+  );
+  gulp.watch('src/js/app.js', gulp.series(jsProcess, browserSyncReload));
+  gulp.watch('src/img/**/*.*', gulp.series(imgProcessWatch, browserSyncReload));
+  gulp.watch('src/fonts/*.*', gulp.series(fontsProcess, browserSyncReload));
 }
 
 // Определение основных переменных
 const build = gulp.series(
-    clean,
-    gulp.parallel(htmlProcess, cssProcess, scssProcessBuild, libsJsProcess, jsProcessBuild, fontsProcess, imgProcessBuild, imgProcessWebp)
+  clean,
+  gulp.parallel(
+    htmlProcess,
+    cssProcess,
+    scssProcessBuild,
+    libsJsProcess,
+    jsProcessBuild,
+    fontsProcess,
+    imgProcessBuild,
+    imgProcessWebp,
+  ),
 );
 
 const dev = gulp.series(
-    clean,
-    gulp.parallel(htmlProcess, cssProcess, scssProcess, libsJsProcess, jsProcess, fontsProcess, imgProcessBuild, imgProcessWebp)
+  clean,
+  gulp.parallel(
+    htmlProcess,
+    cssProcess,
+    scssProcess,
+    libsJsProcess,
+    jsProcess,
+    fontsProcess,
+    imgProcessBuild,
+    imgProcessWebp,
+  ),
 );
 
+const getVersion = () => {
+  console.log('Версия сборки: ', appVersion);
+};
+
 const watchEvent = gulp.series(
-    clean,
-    gulp.parallel(htmlProcess, cssProcess, scssProcess, libsJsProcess, jsProcess, fontsProcess, imgProcessWatch)
+  clean,
+  gulp.parallel(
+    htmlProcess,
+    cssProcess,
+    scssProcess,
+    libsJsProcess,
+    jsProcess,
+    fontsProcess,
+    imgProcessWatch,
+    getVersion,
+  ),
 );
 
 const watch = gulp.parallel(watchEvent, watchFiles, browserSyncInit);
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,91 @@
 $(function() {
   objectFitImages();
 });
+
+var modal = document.getElementById('myModal');
+
+var img = document.getElementById('myImgs');
+var modalImg = document.getElementById('img01');
+var captionText = document.getElementById('caption');
+var body = document.body;
+var span = document.getElementsByClassName('modal-gallery__close')[0];
+let logo = 'img/logo.png';
+let logotwo = 'img/logo_color.png ';
+var hamburger = document.querySelector('.hamburger');
+var mobmenu = document.querySelector('.nav__right');
+const anchors = document.querySelectorAll('#scroll');
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute('href');
+
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
+
+hamburger.addEventListener('click', function() {
+  hamburger.classList.toggle('is-active');
+  mobmenu.classList.toggle('nav__right--visible');
+});
+
+let clientPosition = 0;
+
+const setlassToMenu = (e) => {
+  console.log(e);
+  if (e.clientX > clientPosition) {
+    e.target.classList.add('nav__item--to-left'); //
+  } else {
+    e.target.classList.add('nav__item--to-right');
+  }
+  clientPosition = e.clientX;
+  e.target.classList.add('nav__item--hovered');
+};
+
+const removelassFromMenu = (e) => {
+  e.target.classList.remove('nav__item--to-left'); //
+
+  e.target.classList.remove('nav__item--to-right');
+
+  e.target.classList.remove('nav__item--hovered');
+};
+
+document.querySelectorAll('.nav__item').forEach((el) => {
+  el.addEventListener('mouseenter', setlassToMenu);
+  el.addEventListener('mouseleave', removelassFromMenu);
+});
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 80 ||
+    document.documentElement.scrollTop > 280
+  ) {
+    document.getElementById('navbar').classList.add('white');
+    document.getElementById('logo').src = 'img/logo_color.png';
+  } else {
+    document.getElementById('navbar').classList.remove('white');
+    document.getElementById('logo').src = 'img/logo.png';
+  }
+}
+
+span.onclick = function() {
+  modal.style.display = 'none';
+  body.style.overflowY = 'visible';
+};
+
+document.getElementById('tomatosRow').addEventListener('click', (e) => {
+  if (e.target.classList.contains('container-gallery__item--img')) {
+    modalImg.src = e.target.src;
+    modal.style.display = 'block';
+    body.style.overflowY = 'hidden';
+  }
+  e.preventDefault();
+});
